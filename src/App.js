206c429a05c42a55bc113cf07e0e0c8bfadff144
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2021-12-24 10:27:11 -0300

    new cmt

--- src/App.js
+++ src/App.js
@@ -11,34 +11,73 @@ function Quad(props) {
   );
 }
 
+function Input(props) {
+  const [playername, setPlayername] = useState({ one: "", two: "" });
+
+  return (
+    <form onSubmit={(e) => props.inp(e, playername)}>
+      <div className="inputDiv">
+        <label>Jogador 1</label>
+        <input
+          type="text"
+          value={playername.one}
+          onChange={(e) =>
+            setPlayername({ one: e.target.value, two: playername.two })
+          }
+        />
+      </div>
+
+      <div className="inputDiv">
+        <label>Jogador 2</label>
+        <input
+          type="text"
+          value={playername.two}
+          onChange={(e) =>
+            setPlayername({ one: playername.one, two: e.target.value })
+          }
+        />
+      </div>
+
+      <button>ok</button>
+    </form>
+  );
+}
+
 function Table() {
   const [jogo, setJogo] = useState(["", "", "", "", "", "", "", "", ""]);
 
-  const [jogador1, setJogador1] = useState(true);
-
-  // const calcWinner = () => {
-  //   const compare = [jogo[0], jogo[1], jogo[2]];
-  //   console.log(compare);
-  // };
+  const [playername, setPlayername] = useState({ one: "", two: "" });
+  const [eo1, setEo1] = useState(true);
 
   const handleClk = (data) => {
     var newJogo = [...jogo];
-    newJogo[data.target.id] = jogador1 ? "T" : "M";
+    newJogo[data.target.id] = eo1
+      ? playername.one.substring(0, 2)
+      : playername.two.substring(0, 2);
 
     setJogo(newJogo);
-    setJogador1(!jogador1);
+    setEo1(!eo1);
   };
+
+  function handleInput(event, userinp) {
+    event.preventDefault();
+    setPlayername(userinp);
+  }
 
   return (
     <>
-      <h1>Jogador atual: {jogador1 ? "Thomas" : "Marcelo"}</h1>
+      <Input inp={handleInput} />
+      <h1>Jogador atual: {eo1 ? playername.one : playername.two}</h1>
 
       <div className="head">
         <h2>
           Jogo da velha do <br /> Marcelo e do Thomas
         </h2>
 
-        <button onClick={() => setJogo(["", "", "", "", "", "", "", "", ""])}>
+        <button
+          className="but"
+          onClick={() => setJogo(["", "", "", "", "", "", "", "", ""])}
+        >
           Limpar
         </button>
       </div>
